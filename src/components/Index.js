737224e702ca1831a9d14a012df519e00ce4e596
Shivam Gaur <shivamgaur865029@gmail.com>
2025-09-30 12:50:30 +0530

    FirstCommit

---
+++ src/components/Index.js
@@ -0,0 +1,36 @@
+
+import Header from './Header/header'
+import Footer from './Footer/footer'
+import Container from './container/Container'
+import Logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+import PostCard from './PostCard'
+import Login from './Login'
+import SignUp from  './SignUp'
+import AuthLayout from './AuthLayout'
+import RTE from './RTE'
+import PostForm from './postForm/PostForm'
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard,
+    Login,
+    SignUp,
+    AuthLayout,
+    RTE,
+    PostForm,
+
+}
+
+
